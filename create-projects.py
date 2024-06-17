2366c4537260674013816634985b6c0af3e8f833
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-16 21:58:14 -0400

    [update] imports

--- create-projects.py
+++ create-projects.py
@@ -1,6 +1,5 @@
-import requests # pip install requests
+import gitlab # pip install python-gitlab
 import sys
-import json
 
 gitlab_url = "https://gitlab.instance.net"
 private_token = "your-private-token" # insert private token here
