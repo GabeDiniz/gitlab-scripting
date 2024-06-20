47d45727115464eb16e07314dfd4f4c3be176c57
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-20 18:45:42 -0400

    [create] enable-lfs.py

---
+++ enable-lfs.py
@@ -0,0 +1,14 @@
+import requests # pip install requests
+
+'''
+Description: 
+Usage:
+
+Problem: 
+Solution: 
+'''
+
+gitlab_url = "https://gitlab.instance.net"
+private_token = "your-private-token" # insert private token here
+projectID = "12642" # your projectID
+track = "zip"
